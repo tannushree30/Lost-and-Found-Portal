986f9dc347ed90fdfb64fe29b79000967ec0d495
tannushree30 <rajputtannushree25@gmail.com>
2026-08-10 13:45:09 +0530

    Fix Backend

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.use("/api/ai", aiRoutes);
 // Serve React frontend
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
