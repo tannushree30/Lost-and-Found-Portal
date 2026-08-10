166ae313ed251f5ef000d1ec9f8c76f9e49c7687
tannushree30 <rajputtannushree25@gmail.com>
2026-08-10 14:03:22 +0530

    Fix frontend

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import API from "./Services/api";
-import "./Home.css";
+import "./home.css";
 
 function Home() {
   const navigate = useNavigate();
